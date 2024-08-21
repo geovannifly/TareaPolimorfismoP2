950f2003263c132b9c313658a914c2aad475efee
GeovanniFly <geovanni.fly@gmail.com>
2024-08-20 18:51:42 -0600

    cracion de proyecto

---
+++ TareaPolimorfismo/src/main/java/com/mycompany/tareapolimorfismo/TareaPolimorfismo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareapolimorfismo;
+
+/**
+ *
+ * @author cpere
+ */
+public class TareaPolimorfismo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
